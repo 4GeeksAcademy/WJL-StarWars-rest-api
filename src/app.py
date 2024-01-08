8b97bec94dff7f819235ee9a3e6775d3310545ae
Winston J Lamus <138252771+WinstonLesuT@users.noreply.github.com>
2024-01-08 10:51:06 +0000

    revisar favoritos

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Vehicles, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -52,7 +52,95 @@ def get_users():
 
     return jsonify(users_list), 200
 
+#GET CHARACTERS
 
+@app.route('/characters', methods=['GET'])
+def get_characters():
+
+    allCharacters= Characters.query.all()
+    characters_list= [element.serialize() for element in allCharacters]
+
+    return jsonify(characters_list), 200
+
+@app.route('/characters/<int:characters_id>', methods=['GET'])
+def get_one_characters(characters_id):
+
+    #Obtener solo un dato de una tabla basado en su llave primaria con ".query.get"
+    ##Fetch one single record based on its primary key using ".query.get()"
+    
+    oneCharacter= Characters.query.get(characters_id)
+
+    #Colocar "if condition" para evitar el error "AttributeError: 'NoneType'""
+    ##Use "if condition" to avoid "AttributeError: 'NoneType' object has no attribute 'serialize'""
+    
+    if oneCharacter is None:
+        return 'Character not found', 404
+    return jsonify(oneCharacter.serialize()), 200
+
+#GET PLANETS
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    allPlanets= Planets.query.all()
+    planets_list= [element.serialize() for element in allPlanets]
+
+    return jsonify(planets_list), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def get_one_planets(planets_id):
+    
+    onePlanet= Planets.query.get(planets_id)
+    
+    if onePlanet is None:
+        return 'Planet not found', 404
+    return jsonify(onePlanet.serialize()), 200
+
+#GET VEHICLES
+
+@app.route('/vehicles', methods=['GET'])
+def get_vehicles():
+
+    allVehicles= Vehicles.query.all()
+    vehicles_list= [element.serialize() for element in allVehicles]
+
+    return jsonify(vehicles_list), 200
+
+@app.route('/vehicles/<int:vehicles_id>', methods=['GET'])
+def get_one_vehicles(vehicles_id):
+    
+    oneVehicle= Vehicles.query.get(vehicles_id)
+    
+    if oneVehicle is None:
+        return 'Vehicle not found', 404
+    return jsonify(oneVehicle.serialize()), 200
+
+#FAVORITE
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+
+    favoritos=Favorite.query.all()  
+    favorite_list=[element.serialize() for element in favoritos]
+
+    return jsonify(favorite_list), 200
+
+#ADD FAVORITES
+
+@app.route('/favorites/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    # data = request.get_json()
+    # planet_id = data['planet_id']
+
+    # planet = Planets.query.get(planet_id)
+    new_favorite = Favorite(planet_id=planet_id)
+
+    db.session.add(new_favorite)
+    db.session.commit()
+
+    response_body={'msg':"Planeta ha sido agregado a favoritos"}
+
+    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
